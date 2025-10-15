a2a59e0a898cae49c6af4ef7e7168d136f1fe15b
Khizri Makhmudov <khizrim@khizrim.ru>
2025-10-15 11:50:58 +0300

    fix: add fallback URL for API base URL in client configuration to ensure functionality when environment variable is not set

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -2,7 +2,7 @@ import createClient from 'openapi-fetch';
 
 import type { paths } from './v1';
 
-const baseUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/wp-json`;
+const baseUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL || 'https://backend.ultrastore.khizrim.online'}/wp-json`;
 
 const client = createClient<paths>({ baseUrl });
 
