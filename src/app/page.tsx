cd232dfaf636c98e26f7b67e78f89a7c10463068
Khizri Makhmudov <khizrim@khizrim.ru>
2025-10-19 19:09:46 +0300

    feat: add Reviews component to homepage and enhance responsive design for reviews and long-button styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { InfoBlock } from '@/components/info-block';
 import { PopularCategories } from '@/components/popular-categories';
 // import { ProductLevel } from '@/components/products';
 // import { PromoBanners } from '@/components/promo-banners';
-// import { Reviews } from '@/components/reviews';
+import { Reviews } from '@/components/reviews';
 import { getHomepageData } from '@/shared/api/getHomepageData';
 
 export default async function HomePage() {
-  const { popular_categories, info_blocks } = await getHomepageData();
+  const { popular_categories, info_blocks, reviews } = await getHomepageData();
 
   const [firstInfoBlock, secondInfoBlock] = info_blocks || [];
 
@@ -17,6 +17,7 @@ export default async function HomePage() {
       <PopularCategories items={popular_categories} />
       <InfoBlock {...firstInfoBlock} />
       <InfoBlock {...secondInfoBlock} />
+      <Reviews items={reviews} />
     </>
   );
 }
