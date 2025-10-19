64b461ed11d7f5b5583ef366346b49f8a22ccb0d
Khizri Makhmudov <khizrim@khizrim.ru>
2025-10-19 08:25:06 +0300

    refactor: improve Logo component by adding priority prop and standardizing quotes

--- src/components/ui/logo/logo.tsx
+++ src/components/ui/logo/logo.tsx
@@ -1,54 +1,60 @@
-"use client";
+'use client';
 
-import type { CSSProperties } from "react";
+import type { CSSProperties, MouseEventHandler } from 'react';
 
-import Image from "next/image";
-import type { LinkProps } from "next/link";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import Image from 'next/image';
+import type { LinkProps } from 'next/link';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-import CompactLogo from "@/shared/images/compact-logo.svg";
-import FullLogo from "@/shared/images/full-logo.svg";
-import type { WithClassName } from "@/shared/types/utils";
+import CompactLogo from '@/shared/images/compact-logo.svg';
+import FullLogo from '@/shared/images/full-logo.svg';
+import type { WithClassName } from '@/shared/types/utils';
 
-import styles from "./logo.module.css";
+import styles from './logo.module.css';
 
-interface LogoProps extends Pick<LinkProps, "href"> {
-  title?: string;
-  className?: string;
-  full?: boolean;
-  onClick?: React.MouseEventHandler;
-  width?: number | string;
-  height?: number | string;
+interface LogoProps extends Pick<LinkProps, 'href'> {
+  title?: string
+  className?: string
+  full?: boolean
+  onClick?: MouseEventHandler
+  width?: number | string
+  height?: number | string
+  priority?: boolean
 }
 
-export const Logo = ({ href, title, full, onClick, className, width, height }: WithClassName<LogoProps>) => {
+export const Logo = ({
+  href,
+  title,
+  full,
+  onClick,
+  className,
+  width,
+  height,
+  priority,
+}: WithClassName<LogoProps>) => {
   const pathname = usePathname();
 
   const linkStyle: CSSProperties & Record<string, string> = {};
 
   if (width) {
-    linkStyle["--logo-width"] = typeof width === "number" ? `${width}px` : width;
+    linkStyle['--logo-width'] = typeof width === 'number' ? `${width}px` : width;
   }
 
   if (height) {
-    linkStyle["--logo-height"] = typeof height === "number" ? `${height}px` : height;
+    linkStyle['--logo-height'] = typeof height === 'number' ? `${height}px` : height;
   }
 
   return (
     <Link
       href={href}
-      className={`${styles.link} ${className || ""}`}
+      className={`${styles.link} ${className || ''}`}
       title={title}
-      aria-disabled={pathname === "/"}
+      aria-disabled={pathname === '/'}
       onClick={onClick}
       style={linkStyle}
     >
-      {full ? (
-        <Image src={FullLogo} alt="Full Logo" className={styles.image} />
-      ) : (
-        <Image src={CompactLogo} alt="Compact Logo" className={styles.image} />
-      )}
+      <Image src={full ? FullLogo : CompactLogo} alt="Ultrastore" className={styles.image} priority={priority} />
     </Link>
   );
 };
